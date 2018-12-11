bbe4d60758c00c9c77bd853232be516e79ddaed2
Steve <steven-s-47@hotmail.com>
2018-12-11 17:50:21 -0500

    Changes to recognize the hostname.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+import socket
 
 app = Flask(__name__)
 
 @app.route('/hello/<value>')
 def hello_world(value):
-    response = 'Hello ' + value
+    response = 'Hello ' + value + ' from ' + str(socket.gethostname())
     return jsonify({'message': response})
