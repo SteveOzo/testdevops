b65adc651fd2c3c58e5fb7d13c1e09cf911b78b8
Steve <steven-s-47@hotmail.com>
2018-12-11 17:55:17 -0500

    Creation of the test for the app.

--- test_for_app.py
+++ test_for_app.py
@@ -1,5 +1,7 @@
 import pytest
-from hello import *
+import socket
+from app import *
+
 
 @pytest.fixture
 def client():
@@ -9,5 +11,5 @@ def client():
 def test_root(client):
     """Test the default route."""
 
-    res = client.get('/hello/hola')
-    assert b'Hello' in res.data
+    res = client.get('/hello/Juan')
+    assert 'Juan' in str(res.data) and str(socket.gethostname()) in str(res.data)
