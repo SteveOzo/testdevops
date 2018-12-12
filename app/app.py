deb4b083bb6b9928f9a2e3c0a9d605cf6e669baa
Steve <steven-s-47@hotmail.com>
2018-12-11 19:22:22 -0500

    Files for Docker Compose.

--- app/app.py
+++ app/app.py
@@ -8,3 +8,7 @@ app = Flask(__name__)
 def hello_world(value):
     response = 'Hello ' + value + ' from ' + str(socket.gethostname())
     return jsonify({'message': response})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9999)
